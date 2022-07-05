59f40f6129d418cc5a33699f3fa3910eed39beba
dhuson <dhuson@davidhuson.dev>
2022-07-05 10:47:08 -0500

    feat(darkMode): configured dark mode via tailwind class method

--- src/nothing.tsx
+++
@@ -1,9 +0,0 @@
-function Nothing() {
-  return (
-    <div>
-      <h1 className="text-red-900">Nothing</h1>
-    </div>
-  )
-}
-
-export default Nothing
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
