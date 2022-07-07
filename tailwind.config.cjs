1840fa966b1750ee88f3d8ae55c0f21c967962f3
dhuson <dhuson@davidhuson.dev>
2022-07-07 09:48:28 -0500

    style: add card image and modify theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#101218',
+      },
+    },
   },
   plugins: [],
 }
