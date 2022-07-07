994d9eb79d067565948335588ae70b9dbb8d5150
dhuson <dhuson@davidhuson.dev>
2022-07-07 11:33:59 -0500

    fix: sanity client API version format again

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,8 +3,8 @@ import sanityClient from '@sanity/client'
 const config = {
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || 'j6vs6osg',
   dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
-  token: import.meta.env.PUBLIC_SANITY_READ_TOKEN || 'v2021-12-21',
-  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION,
+  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION || 'v2021-12-21',
+  token: import.meta.env.PUBLIC_SANITY_READ_TOKEN,
   useCdn: true,
 }
 
